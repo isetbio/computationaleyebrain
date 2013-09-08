a320753bbc5d81061289fb2c20f823b28053e10a
Brian A. Wandell <wandell@stanford.edu>
2013-09-07 18:09:40 -0700

    Start of simplification.

--- colorContours/validation/v_ptbIsetIrradiance.m
+++ colorContours/validation/v_ptbIsetIrradiance.m
@@ -1,7 +1,7 @@
 %% v_PTBISETBIOIrradiance
 %
+% Compare iset and ptb irradiance calculations
 %
-%  Plot comparison of iset and ptb irradiance, optionally
 %
 % PTB, conversion is pupilArea/(eyeLength^2). (pi /(1 + 4*fN^2*(1+abs(m))^2)
 %
@@ -9,42 +9,90 @@
 % computed at this level.
 %if (runtimeParams.DO_SIM_PLOTS)
 
+%% 
+s_initISET
+
+monitorName = 'LCD-Apple.mat';
+wave   = 380:4:780;
+bLevel = 0.5;    % Linear value of display primary
+
+%% Create human optics with standard wave front aberrations
+
+wvf    = wvfCreate('wave',wave);
+
+pupilDiameterMm = 3;   
+sample_mean = wvfLoadThibosVirtualEyes(pupilDiameterMm);
+wvf    = wvfSet(wvf,'zcoeffs',sample_mean);
+
+wvf    = wvfComputePSF(wvf);
+oiB    = wvf2oi(wvf,'shift invariant');
+
 %% ISETBIO formulation
 
-scene = sceneFromFile('backFile.png','rgb',[],'LCD-Apple');
-vcAddAndSelectObject(scene); sceneWindow;
+% Write an image file with a gray background.  We want it set to one half
+% the max primary intensity.  To do this, we need to read the DAC for the
+% display calculate the right level.
 
-%% Create standard human polychromatic PSF using wavefront tools
-% Make it an iset OI thingy.
-pupilDiameterMm = 3;   
-wave = 380:4:780;
-wvf = wvfCreate('wave',wave);
-sample_mean = wvfLoadThibosVirtualEyes(pupilDiameterMm);
-wvf = wvfSet(wvf,'zcoeffs',sample_mean);
-wvf = wvfComputePSF(wvf);
-oiD = wvf2oi(wvf,'shift invariant');
-optics = oiGet(oiD,'optics');
+d       = displayCreate(monitorName);
+gTable  = displayGet(d,'gamma table');  % plot(gTable)
+igTable = ieLUTInvert(gTable);        % Maps linear values to DAC
+bDAC    = ieLUTLinear([0.5,0.5,0.5],igTable)/size(gTable,1);
 
-oiD = oiCompute(oiD,scene);
-vcAddAndSelectObject(oiD); oiWindow;
+bImage = ones(128,128,3);
+for ii=1:3
+    bImage(:,:,ii) = bImage(:,:,ii)*bDAC(ii);
+end
 
-% The difference is because of the magnification difference between ISET
-% and PTB.  See note in doOneSimulation.  Document it here better.
-vcNewGraphWin; plot(uData.y,ptbIrradiance,'.')
-identityLine;
-grid on
+bFile = fullfile(isetbioRootPath,'tmp','bFile.png');
+imwrite(bImage,bFile);
 
+% Build the scene from the image file
+bScene = sceneFromFile(bFile,'rgb',[],monitorName,wave);
+vcAddAndSelectObject(bScene); sceneWindow;
+
+% % Create a scene with a background of [0.5,0.5,0.5]
+% scene = sceneFromFile('backFile.png','rgb',[],'LCD-Apple');
+% vcAddAndSelectObject(scene); sceneWindow;
+
+%% ISETBIO path for creating an irradiance
+
+oiB    = oiCompute(oiB,bScene);
+vcAddAndSelectObject(oiB); oiWindow;
+
+%%
+sz = oiGet(oiB,'size');
+rect = [sz(2)/2,sz(1)/2,5,5];
+roiLocs = ieRoi2Locs(rect);
+
+ibIrradiance = vcGetROIData(oiB,roiLocs,'energy');
+
+ibIrradiance = mean(ibIrradiance,1);
+vcNewGraphWin; plot(wave,ibIrradiance); grid on; title('ISET irradiance')
 
 %% PTB computes the ptbIrradiance w/m2
-d = displayCreate('LCD-Apple');
+d       = displayCreate(monitorName);
+wave    = displayGet(d,'wave');
+
 backRGB = [0.5, 0.5, 0.5]';                    % Define background for experment in monitor RGB
 backSpd = displayGet(d,'spd')*backRGB;
-wave    = displayGet(d,'wave');
-focalLengthMm = opticsGet(optics,'focal length','mm');
-integrationTimeSec = 0.05;
-[~,~,ptbPhotoreceptors,ptbIrradiance] = ptbConeIsomerizationsFromSpectra(backSpd,wave,...
+
+optics = oiGet(oiB,'optics');
+focalLengthMm   = opticsGet(optics,'focal length','mm');
+pupilDiameterMm = opticsGet(optics,'pupil diameter','mm');
+integrationTimeSec = 0.05;  % Irrelevant for irradiance
+
+[~,~,ptbPhotoreceptors,ptbIrradiance] = ...
+    ptbConeIsomerizationsFromSpectra(backSpd,wave,...
     pupilDiameterMm,focalLengthMm,integrationTimeSec,0);
-vcNewGraphWin; plot(wave,ptbIrradiance); grid on
+
+vcNewGraphWin; plot(wave,ptbIrradiance); grid on; title('PTB irradiance')
+
+%% Compare
+% The difference is because of the magnification difference between ISET
+% and PTB.  See note in doOneSimulation.  Document it here better.
+vcNewGraphWin; plot(ibIrradiance,ptbIrradiance,'.')
+identityLine;
+grid on
 
 %%
 if (0)
