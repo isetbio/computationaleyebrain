4cd6d00793cad61c8fc781493809f47c2b04ff7e
Haomiao Jiang <hjiang36@gmail.com>
2013-11-17 13:35:16 -0800

    Add DCT quantization test

---
+++ s_DCTCoefQuantization.m
@@ -0,0 +1,16 @@
+%% s_DCTCoefQuantization
+%    This script uses iset bio to test the just noiticeable difference for
+%  quantization error in DCT coefficients. The experiment design is the
+%  same as Watson's idea.
+%
+%  (HJ) Nov, 2013
+
+%% Create Scene
+
+
+%% Compute optical image
+
+
+%% Create cone structure
+
+%% Compute cone absorption and do classification
