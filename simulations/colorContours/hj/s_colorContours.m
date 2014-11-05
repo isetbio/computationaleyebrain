ebc52b85c0010f07560d09c072c891e697a4d7ed
Haomiao Jiang <hjiang36@gmail.com>
2014-11-05 12:20:11 -0800

    color contour for protan

--- simulations/colorContours/hj/s_colorContours.m
+++ simulations/colorContours/hj/s_colorContours.m
@@ -6,7 +6,7 @@
 %% Init parameters
 s_initISET;
 ref     = [0 0 0];
-dirList = [0 15 25 30 40 42 45 47 50 55 65 135];  % directions
+dirList = [15 25 30 40 42 45 47 50 55 65 90 115 135 150];  % directions
 dirList = [dirList dirList - 180];
 cropSz  = 12;
 
@@ -15,7 +15,7 @@ cropSz  = 12;
 cmd = '[thresh, expData] = ccThreshold(ref, dirList(jobindex), params);';
 cmd = [cmd 'save(sprintf(''~/ccContour%d.mat'',jobindex));'];
 params.ccParams.cropSz = cropSz;
-params.ccParams.rgbDensities = [0.3 0.6 0 0.1];
+params.ccParams.rgbDensities = [0.6 0 0.3 .1];
 
 try % try use proclus
     sgerun2(cmd, 'colorContour', 1, 1:length(dirList));
