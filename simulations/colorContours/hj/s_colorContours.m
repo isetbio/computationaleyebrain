8c8d7f5227f2b82297262f2650bddd25e546755e
hjiang36 <hjiang36@gmail.com>
2014-11-11 21:08:06 -0800

    parameter change in s_colorContours

--- simulations/colorContours/hj/s_colorContours.m
+++ simulations/colorContours/hj/s_colorContours.m
@@ -6,16 +6,16 @@
 %% Init parameters
 s_initISET;
 ref     = [0 0 0];
-dirList = [15 25 30 40 42 45 47 50 55 65 90 115 135 150];  % directions
+dirList = [0 15 25 30 40 42 45 47 50 55 65 90 115 135 150];  % directions
 dirList = [dirList dirList - 180];
-cropSz  = 12;
+cropSz  = 22;
 
 %% Compute classification accuracy
 %  Set up proclus command
 cmd = '[thresh, expData] = ccThreshold(ref, dirList(jobindex), params);';
 cmd = [cmd 'save(sprintf(''~/ccContour%d.mat'',jobindex));'];
 params.ccParams.cropSz = cropSz;
-params.ccParams.rgbDensities = [0.6 0 0.3 .1];
+params.ccParams.rgbDensities = [0 0.6 0.3 .1];
 
 try % try use proclus
     sgerun2(cmd, 'colorContour', 1, 1:length(dirList));
