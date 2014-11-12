f260951745570c468ed309541453522b965ab4de
Haomiao Jiang <hjiang36@gmail.com>
2014-11-11 22:19:33 -0800

    Simplify wavelength discrimination code

--- simulations/Wavelength Sensitivity/s_wavelengthDiscrimination.m
+++ simulations/Wavelength Sensitivity/s_wavelengthDiscrimination.m
@@ -1,23 +1,25 @@
-%% wavelengthDiscrimination
-%   Compute sensitivity for different color
-%   The computation is done in cone absorption level, openency and rgc
-%   features are not included
+%% s_waveDiscrimination_Proclus
+%    This script helps run wavelength discrimination in parallel on
+%    Proclus
 %
-%  (HJ) March, 2014
+%    If proclus is not available, it will run locally instead.
+%
+%  (HJ) ISETBIO TEAM, 2014
 
-%% Compute wavelength discrimination
-refWave = 480:10:580;
-jndWave = zeros(length(refWave), 1);
+refWave = 400:10:700;
 
-for ii = 1 : length(refWave)
-    fprintf('refWave:%d\t\t', refWave(ii));
-    [jndWave(ii), acc, err] = coWaveDiscrimination([refWave(ii) refWave(ii)+0.01]);
-    fprintf('jndWave:%.1f\t dist:%.1f\n', jndWave(ii), jndWave(ii)-refWave(ii));
+try % try using proclus to accelerate computation
+    cmd = ['[jndWave, expData] =' ...
+      'coWaveDiscrimination([refWave(jobindex) refWave(jobindex)+0.01]);'];
+    cmd = [cmd 'save(sprintf(''~/waveDisc%d.mat'', jobindex));'];
+    sgerun2(cmd,'waveDiscrimination',1, 1:length(refWave));
+catch
+    jndWave = zeros(length(refWave), 1);
+    for ii = 1 : length(refWave)
+        fprintf('refWave:%d\t\t', refWave(ii));
+        [jndWave(ii), expData] =  ...
+            coWaveDiscrimination([refWave(ii) refWave(ii)+0.01]);
+        fprintf('jndWave:%.1f\t dist:%.1f\n', ...
+                 jndWave(ii), jndWave(ii)-refWave(ii));
+    end
 end
-
-%% Plot
-
-
-
-
-%% END
