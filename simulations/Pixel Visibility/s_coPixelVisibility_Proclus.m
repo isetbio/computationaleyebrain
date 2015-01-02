2769d4fedc941b277a9837cf78f9794075aa8390
Haomiao Jiang <hjiang36@gmail.com>
2015-01-02 13:06:26 -0800

    add s_coPixelVisibility

---
+++ simulations/Pixel Visibility/s_coPixelVisibility_Proclus.m
@@ -0,0 +1,9 @@
+%% s_coPixelVisibility
+%    Compute threshold for just noticeable viewing distance to distinguish
+%    uniform scene and pixeleted scene
+%
+%  (HJ) ISETBIO TEAM, 2015
+
+%% Init parameters
+ieInit;
+warning('NYI');
