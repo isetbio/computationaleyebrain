6798126f528d2e014d7a31d0929e3559d83e28d7
Haomiao Jiang <hjiang36@gmail.com>
2015-04-12 21:25:22 -0700

    Update t_displayPixelLayoutComparison to show impact to fonts

--- tutorial/t_displayPixelLayoutComparison.m
+++ tutorial/t_displayPixelLayoutComparison.m
@@ -66,3 +66,42 @@ for ii = 1 : nDist
     subplot(nDist, 3, 3 + 3*(ii-1)); imshow(oiGet(oi, 'rgb image'));
     drawnow; title(sprintf('OLED-Sony (%.2f m)', vd(ii)));
 end
+
+%% Font on different display
+font = fontCreate; % letter g
+displayName = {'CRT-Dell', 'crt', 'Dell-Chevron', 'LCD-HP', 'OLED-Sony'};
+nd = length(displayName);
+
+oi = oiCreate('human');
+
+vcNewGraphWin;
+for ii = 1 : nd
+    % create display
+    d = displayCreate(displayName{ii});
+    
+    % show sub-pixel image
+    subplot(2 + nDist, nd, ii);
+    dmap = displayGet(d, 'dixel intensity map');
+    dmap = dmap(:, :, 1:3); % use RGB primaires only
+    imshow(dmap / max(dmap(:))); title(displayGet(d, 'name')); drawnow;
+    
+    % create and show font scene
+    scene = sceneFromFont(font, d);
+    subplot(2 + nDist, nd, ii + nd);
+    imshow(sceneGet(scene, 'rgb image')); drawnow;
+    
+    % compute opitcal image at different viewing distance
+    width = sceneGet(scene, 'width');
+    for jj = 1 : nDist
+        % adjust scene field of view according to viewing distance
+        scene = sceneSet(scene, 'distance', vd(jj));
+        scene = sceneSet(scene, 'h fov', atand(width / vd(jj)));
+        
+        % compute optical image
+        oi = oiCompute(oi, scene);
+        
+        % show optical image
+        subplot(2+nDist, nd, ii + nd * (jj + 1));
+        imshow(oiGet(oi, 'rgb image')); drawnow;
+    end
+end
