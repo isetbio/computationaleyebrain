fa7bf26ba9330d6b7215b382ad9fbc58155ea839
Brian Wandell <wandell@stanford.edu>
2015-04-23 17:54:18 -0700

    Beginning scripts for two projects

---
+++ scripts/s_Austin.m
@@ -0,0 +1,25 @@
+
+%% Useful scripts to show
+%
+% I didn't really do any of this with the Austin people.
+% I just talked with them.
+% It would be good to have a script and to produce examples of what code
+% look like.
+s_sceneFromMultispectral
+
+t_opticsAiryDisk
+% t_opticsDiffraction
+
+% t_wvfAstigmatism
+% t_wvfZernicke
+
+% t_humanLineSpread
+% t_eyemovement
+
+t_VernierAcuity
+
+%% Explain object sets/gets/create 
+
+%% Github and git
+
+%%
---
+++ simulations/s_compressVisibility.m
@@ -0,0 +1,28 @@
+% Script to analyze the visibility of compression artifacts
+%
+%
+%
+
+img1 = image of a line
+img2 = compressed version of img1
+
+%% set viewing parameters
+
+% Model the display
+
+% viewing distance and other conditions
+
+
+
+%%  Model the human observer
+
+% Choose a model of the optics
+
+% Decide on eye movements
+
+% render cone mosaics 
+
+%% Run classifiers on them.
+
+
+%% End
