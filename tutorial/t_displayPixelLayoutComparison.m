fff7f07ec7221fcb40ca2594025d553f43db8ef1
Haomiao Jiang <hjiang36@gmail.com>
2015-05-05 01:32:27 -0700

    update t_displayPixelLayoutComparison

--- tutorial/t_displayPixelLayoutComparison.m
+++ tutorial/t_displayPixelLayoutComparison.m
@@ -69,6 +69,9 @@ end
 
 %% Font on different display
 font = fontCreate; % letter g
+font.bitmap = 1 - font.bitmap; % flip it to be white letter on black
+padsz = [5 5]; padval = 0; % padding size and val for sceneFromFont
+
 displayName = {'CRT-Dell', 'crt', 'Dell-Chevron', 'LCD-HP', 'OLED-Sony'};
 nd = length(displayName);
 
@@ -86,7 +89,7 @@ for ii = 1 : nd
     imshow(dmap / max(dmap(:))); title(displayGet(d, 'name')); drawnow;
     
     % create and show font scene
-    scene = sceneFromFont(font, d);
+    scene = sceneFromFont(font, d, [], [], padsz, padval);
     subplot(2 + nDist, nd, ii + nd);
     imshow(sceneGet(scene, 'rgb image')); drawnow;
     
@@ -103,5 +106,75 @@ for ii = 1 : nd
         % show optical image
         subplot(2+nDist, nd, ii + nd * (jj + 1));
         imshow(oiGet(oi, 'rgb image')); drawnow;
+    end
+end
+
+%% Show scene radiance plot
+vcNewGraphWin;
+for ii = 1 : nd
+    % create display
+    d = displayCreate(displayName{ii});
+    
+    % show sub-pixel image
+    subplot(3, nd, ii);
+    dmap = displayGet(d, 'dixel intensity map');
+    dmap = dmap(:, :, 1:3); % use RGB primaires only
+    imshow(dmap / max(dmap(:))); title(displayGet(d, 'name')); drawnow;
+    
+    % create and show font scene
+    scene = sceneFromFont(font, d, [], [], padsz, padval);
+    subplot(3, nd, ii + nd);
+    imshow(sceneGet(scene, 'rgb image')); drawnow;
+    
+    % compute opitcal image at different viewing distance
+    [uData,hg] = plotScene(scene, 'radiance hline', [1 230]); close(hg);
+    subplot(3, nd, ii + 2*nd);
+    mesh(uData.pos,uData.wave,uData.data)
+    xlabel('Position (mm)'); ylabel('Wavelength (nm)');
+    zlabel('Radiance (q/s/nm/m^2)');
+end
+
+%% Show OI irradiance plot
+sampleWave = [450 550 650];
+for jj = 1 : nDist
+    vcNewGraphWin;
+    for ii = 1 : nd
+        % create display
+        d = displayCreate(displayName{ii});
+        
+        % show sub-pixel image
+        subplot(6, nd, ii);
+        dmap = displayGet(d, 'dixel intensity map');
+        dmap = dmap(:, :, 1:3); % use RGB primaires only
+        imshow(dmap / max(dmap(:))); title(displayGet(d, 'name')); drawnow;
+        
+        % create and show font scene
+        scene = sceneFromFont(font, d, [], [], padsz, padval);
+        width = sceneGet(scene, 'width');
+        
+        % adjust scene field of view according to viewing distance
+        scene = sceneSet(scene, 'distance', vd(jj));
+        scene = sceneSet(scene, 'h fov', atand(width / vd(jj)));
+        
+        % compute optical image
+        oi = oiCompute(oi, scene);
+        
+        % show optical image
+        subplot(6, nd, ii + nd);
+        imshow(oiGet(oi, 'rgb image')); drawnow;
+        
+        % show optical image at different wavelength
+        for kk = 1 : length(sampleWave)
+            subplot(6, nd, ii + nd * (kk+1));
+            p = oiGet(oi, 'photons', sampleWave(kk));
+            imshow(p / max(p(:)));
+        end
+        
+        % show optical image
+        [uData,hg] = plotOI(oi, 'irradiance hline', [1 288]); close(hg);
+        subplot(6, nd, ii + 5*nd);
+        mesh(uData.pos,uData.wave,uData.data)
+        xlabel('Position (um)');
+        ylabel('Wavelength (nm)'); zlabel('Irradiance (q/s/nm/m^2)');
     end
 end
