4101cb0016403748a2544478fea9c006fa85c6bc
Haomiao Jiang <hjiang36@gmail.com>
2015-05-16 23:24:39 -0400

    update colorblind video script

--- scripts/s_colorblindVideoxy.m
+++ scripts/s_colorblindVideoxy.m
@@ -6,7 +6,7 @@
 
 %% Init
 ieInit;
-wave = 420:5:700;  % wavelength samples in nm
+load('Alpern_data.mat');
 XYZ  = ieReadSpectra('XYZ', wave);
 cbType = 'Tritan'; % color blind type
 n = length(wave);
@@ -16,7 +16,7 @@ vcNewGraphWin; % new figure window
 hold on;
 
 xy = bsxfun(@rdivide, XYZ, sum(XYZ, 2));
-plot([xy(:,1);xy(1,1)], [xy(:,2);xy(1,2)], '--');
+plot([xy(:,1);xy(1,1)], [xy(:,2);xy(1,2)], '--', 'LineWidth', 2);
 
 %% Plot Brettel transform
 wp = ieXYZFromEnergy(1e-3*ones(1, n), wave);
@@ -44,11 +44,30 @@ cbXYZ = squeeze(cbXYZ);
 cbxy_n = bsxfun(@rdivide, cbXYZ, sum(cbXYZ, 2));
 plot(cbxy_n(:,1), cbxy_n(:,2), 'o')
 
-%% 
-vcNewGraphWin; hold on;
+%% Make a video
+hfig = vcNewGraphWin; hold on; grid on;
+plot([xy(:,1);xy(1,1)], [xy(:,2);xy(1,2)], '--', 'LineWidth', 2);
+
+videoObj = VideoWriter('Tritanope.avi');
+videoObj.FrameRate = 2;
+open(videoObj);
+
+% plot Alpern
+lColor = [136 204 238; 221 204 119; 204 102 119]/255;
 for ii = 1 : n
-    plot(xy(ii,1), xy(ii,2), '.b');
-    plot(cbxy_b(ii, 1), cbxy_b(ii, 2), 'or');
-    plot(cbxy_l(ii, 1), cbxy_l(ii, 2), 'xg');
-    drawnow; WaitSecs(0.5);
+    plot(data(ii, 1), data(ii, 2), 'o', 'Color', lColor(1,:));
+    if ii > 1
+        plot(data(ii-1:ii, 1), data(ii-1:ii, 2), ...
+            'LineWidth', 2, 'Color', lColor(1,:))
+    end
+
+    plot(cbxy_b(ii, 1), cbxy_b(ii,2), 'o', 'Color', lColor(2,:));
+
+    plot(cbxy_l(ii, 1), cbxy_l(ii, 2), 'x', 'Color', lColor(3,:));
+    % plot(cbxy_n(ii, 1), cbxy_n(ii, 2), 'x', 'Color', lColor(3,:));
+    
+    axis([0 0.8 0 0.9]);
+    drawnow; writeVideo(videoObj, getframe(hfig));
 end
+
+close(videoObj);
