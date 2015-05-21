46fde407cf33a10f2fdaafb8a115b20ab6145b5d
Brian Wandell <wandell@stanford.edu>
2015-05-20 21:47:44 -0700

    Some bug fixes about parameters, I guess.

--- tutorial/t_displayPixelLayoutSCIELAB.m
+++ tutorial/t_displayPixelLayoutSCIELAB.m
@@ -10,17 +10,20 @@
 %
 %  (HJ) ISETBIO TEAM, 2015
 
-%% Init
+%% Init variables
+
 ieInit;         % init a new isetbio session
-nDist = length(vd);
+
+% Smaller field of view means subject is further away
+fovList = (0.1:0.05:0.8);
 
 % displayName = {'CRT-Dell', 'crt', 'Dell-Chevron', 'LCD-HP', 'OLED-Sony'};
 displayName = {'Dell-Chevron', 'LCD-HP'};
 
-%% Font
-
+% Font
 font = fontCreate;             % letter g
 font.bitmap = 1 - font.bitmap; % flip data to be white letter on black
+padsz = [5 5]; padval = 0;     % padding size and val for sceneFromFont
 
 %% Show the subpixel images, though they are clear in the scene
 
@@ -37,45 +40,44 @@ for ii = 1 : nd
     
 end
 
-%% Make scenes of the same font on the two displays, matching dpi
+%% Make scenes of the same font on the two displays
+
+% Match dpi and spd
 clear scene
-% meanLuminance = 30;
 for ii=1:nd
     d = displayCreate(displayName{ii});
+    
     % Make the SPDs the same
-    if ii == 1
-        spd = displayGet(d,'spd');
-    elseif ii > 1
-        d = displaySet(d,'spd',spd);
+    if ii == 1,    spd = displayGet(d,'spd');
+    elseif ii > 1, d = displaySet(d,'spd',spd);
     end
     
     wp{ii} = displayGet(d,'white point');
     d = displaySet(d,'dpi',fontGet(font,'dpi'));
     
     scene{ii} = sceneFromFont(font, d, [], [], padsz, padval);
+    scene{ii} = sceneSet(scene{ii},'name',displayName{ii});
     % scene{ii} = sceneAdjustLuminance(scene{ii},meanLuminance);
     vcAddObject(scene{ii});
 end
-
 sceneWindow;
-%% Adjust field of view for viewing distance
 
-% fov = sceneGet(scene{1},'fov')
-% Field of view of the letter image.  This includes the black area.
-fovList = (0.1:.1:0.8);
-% fovList = (0.15:0.15:0.6);
-% Given the dpi and fov, figure out the viewing distance
+%% Adjust field of view to set viewing distance
 
+% Below, from the dpi and fov we calculate the viewing distance
 
-meanE = zeros(size(fovList));
+% We set up the mean error and the range of the delta E values
+medianE = zeros(size(fovList));
 rng = zeros(length(fovList),2);
+
+% For each field of view, process
 for ff=1:length(fovList)
     for ii=1:2
         scene{ii} = sceneSet(scene{ii},'fov',fovList(ff));
     end
     
     % Now run scielab on the two scenes
-    sampPerDeg = 1/sceneGet(scene{1},'degrees per sample');
+    sampPerDeg = 1/sceneGet(scene{1},'degrees per sample')
     
     % Run S-CIELAB based on CIELAB 2000
     params.deltaEversion = '2000';
@@ -86,38 +88,39 @@ for ff=1:length(fovList)
     params.filterversion = 'distribution';
     errorImage = scielab(sceneGet(scene{1},'xyz'), sceneGet(scene{2},'xyz'), wp, params);
     
-    % Crop the error image a bit
-    % This might depend on the character.  
+    % Crop the error image a bit to eliminate the padding.
+    % This rect will depend on the character.  
     rect = [30     1   184   266];  % Georgia g, 14
     errorImage = imcrop(errorImage,rect);
 
-    meanE(ff) = mean(errorImage(:));
+    medianE(ff) = median(errorImage(:));
     rng(ff,1) = prctile(errorImage(:),0.1);
     rng(ff,2) = prctile(errorImage(:),0.9);
+    
+    % Show the error images, though this is not all that important
     vcNewGraphWin;
     imagesc(errorImage);
     colorbar;
     
 end
 
-%%
-vcNewGraphWin;
+%% Plot the error as a function of viewing distance (meters)
+
 dpi  = fontGet(font,'dpi');
 cols = sceneGet(scene{1},'cols');
 colsInches = cols/dpi;
 
-a = tan(colsInches/vd);
+% a = tan(colsInches/vd);
 vd = colsInches ./ atan(fovList(:));
 % 0.0254 m/inch
-vd = 0.0254*vd;  % In meters
+vd = 0.0254*vd * 100;  % In centimeters
 
-% p = plot(vd,meanE);
-p = errorbar(vd,meanE,rng(:,1),rng(:,2));
+vcNewGraphWin;
+p = errorbar(vd,medianE,rng(:,1),rng(:,2));
 set(p,'linewidth',2);
-% set(gca,'xtick',[0:.2:1],'ytick',[2:8]);
 grid on
 
-xlabel('Viewing distance (m)')
-ylabel('Mean \Delta E_S')
+xlabel('Viewing distance (cm)')
+ylabel('Median \Delta E_S')
 
 %% END
