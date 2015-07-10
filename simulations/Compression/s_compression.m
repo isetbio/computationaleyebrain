7d4bfa87c578b2519ab7129e69f54901a7955a59
Brian Wandell <wandell@stanford.edu>
2015-07-09 23:12:54 -0700

    Comments and scatter plot

--- simulations/Compression/s_compression.m
+++ simulations/Compression/s_compression.m
@@ -12,37 +12,45 @@ webdir = 'http://scarlet.stanford.edu/validation/SCIEN/ISETBIO/VESA';
 imgList = lsScarlet([webdir '/Full_1080p_ref_images'], '.bmp');
 
 %% Set display and viewing parameters
+
 % Model the display
 d = displayCreate('LCD-Apple');
 
-% Experiment conditions
 % Adjust viewing distance to match pixel per degree
-ppd = 60; % 60 pixels per degree
+ppd = 60;                                  % 60 pixels per degree
 vd  = ppd / displayGet(d, 'dpi') / tand(1) * 0.0254;  % Meter
-d = displaySet(d, 'viewing distance', vd);
+d   = displaySet(d, 'viewing distance', vd);
 invGamma = displayGet(d, 'inverse gamma'); % inverse gamma table
-rgb2xyz = displayGet(d, 'rgb2xyz'); % rgb to xyz tranformation matrix
-wp = displayGet(d, 'white xyz'); % white point
+rgb2xyz  = displayGet(d, 'rgb2xyz');       % rgb to xyz tranformation matrix
+wp = displayGet(d, 'white xyz');           % white point
 
-% eye position and spatial integration size
-pSzDeg  = 0.08; % spatial integration size in degree
-pSzDots = round(pSzDeg * displayGet(d, 'dots per deg')); % convert to dots
+% Eye position and spatial integration size
+pSzDeg  = 0.08;               % spatial integration size in degree
+% Represented here in terms of dots on the display
+pSzDots = round(pSzDeg * displayGet(d, 'dots per deg')); 
+fprintf('Spatial integration %.3f arc minutes (%.3f deg)\n',60*pSzDeg, pSzDeg);
+conesPerDeg = 100;  
+fprintf('This spans a region containing %.1f cones\n', (conesPerDeg*pSzDeg)^2);
 
+% Number of trials
 nTrial = 15;
 
-% Init for accuracy
-acc = zeros(length(imgList), 16); % in shape of n_images, n_algorithms
+% Initialize parameters that will summarize accuracy
+acc = zeros(length(imgList), 16);           % in shape of n_images, n_algorithms
 expAcc = nan(length(imgList), 16);
 cieDeltaE  = zeros(length(imgList), 16);
 scieDeltaE = zeros(length(imgList), 16);
 
-%% Load experiment data
+%% Load experimental data 
+
+%  The behavioral data are stored in the github folder
 expData = importdata('2014_VESA.csv');
 expData.textdata = expData.textdata(2:end, 1);
 
 %% Compute accuracy for algorithms and images
+
 for ii = 1 : length(imgList)
-    % get image name with out '.bmp'
+    % get image name but strip off the extension '.bmp'
     imgName = [webdir '/cropped_images/' imgList(ii).name(1:end-4)];
     
     % load cropped image - original
@@ -61,11 +69,15 @@ for ii = 1 : length(imgList)
         fprintf('%s_x%d:', imgList(ii).name(1:end-4), jj);
         
         % randomize eye position
+        % Can we use the eye movement code in ISETBIO for this?
         pos = floor(bsxfun(@times, rand(nTrial, 2), imgSz - pSzDots)) + 1;
         
         % compute accuracy for each trial
         curAcc = zeros(nTrial, 1);
         for kk = 1 : nTrial
+            
+            % Crop out a portion of the original and compressed image. The
+            % cropped portion corresponds to the eye positions at pos.
             pO = imcrop(imgO, [pos(kk, [2 1]) pSzDots pSzDots]);
             pC = imcrop(imgC, [pos(kk, [2 1]) pSzDots pSzDots]);
             
@@ -93,8 +105,11 @@ for ii = 1 : length(imgList)
         imgC_Linear = ieLUTLinear(imgC, invGamma);
         imgO_XYZ = imageLinearTransform(imgO_Linear, rgb2xyz);
         imgC_XYZ = imageLinearTransform(imgC_Linear, rgb2xyz);
-        de = deltaEab(imgO_XYZ, imgC_XYZ, wp);
-        cieDeltaE(ii, jj) = median(de(:));
+        
+        % Keep this out for a while.  We wil put it back after we move
+        % deltaEab into ISETBIO
+        % de = deltaEab(imgO_XYZ, imgC_XYZ, wp);
+        % cieDeltaE(ii, jj) = median(de(:));
         
         % compue DeltaEab for SCIELab
         % params.sampPerDeg = ppd;
@@ -107,12 +122,16 @@ for ii = 1 : length(imgList)
 end
 
 %% Visualize
-vcNewGraphWin; plot(acc(:), expAcc(:), 'o');
+vcNewGraphWin; 
+% acc and expAcc are different different images and compression levels
+% acc(img,clevel)
+plot(acc(:), expAcc(:), 'o');
 xlabel('Predicted Accuracy'); ylabel('Measurement Accuracy');
 
 save result.mat acc expAcc cieDeltaE scieDeltaE
 
-%% Analyze
+%% Analyze how the predicted and measured difficulties correlate
+
 %  See rank correlation
 indx = ~isnan(expAcc);
 exp_diff = bsxfun(@minus, expAcc(indx), expAcc(indx)');
@@ -123,10 +142,18 @@ rank_corr = sum(exp_diff(:) .* sim_diff(:) >= 0) / numel(exp_diff);
 indx = abs(exp_diff) > 0.05;
 rank_corr_large = sum(exp_diff(indx) .* sim_diff(indx) >= 0)/sum(indx(:));
 
-% Mean and variation of each image
+% Mean and variation accuracy of each image averaged across compression
+% levels.
 sim_imgMean = mean(acc, 2); sim_imgStd = std(acc, [], 2);
 exp_imgMean = nanmean(expAcc, 2); exp_imgStd = nanstd(expAcc, [], 2);
 indx = ~isnan(exp_imgMean);
+
+vcNewGraphWin;
+plot(sim_imgMean(:),exp_imgMean(:),'o')
+grid on;
+xlabel('Simulated'); ylabel('Experiment')
+axis equal; identityLine;
+
 vcNewGraphWin; hold on;
 X = 1:sum(indx); X = X(:);
 bar(X, [sim_imgMean(indx) exp_imgMean(indx)]);
@@ -137,14 +164,23 @@ ax = gca; ax.XTick = X; ax.XTickLabel = {imgList(indx).name};
 ax.XTickLabelRotation = 45;
 legend('ISETBIO Pred', 'Measurement Data');
 
-% Mean and variation of each algorithm
+% Mean and variation of each algorithm averaged across images
 sim_algMean = mean(acc); sim_algStd = std(acc);
 exp_algMean = nanmean(expAcc); exp_algStd = nanstd(expAcc);
 indx = ~isnan(exp_algMean);
+
+vcNewGraphWin;
+title('Algorithm comparisons')
+plot(sim_algMean(:),exp_algMean(:),'o')
+grid on;
+xlabel('Simulated'); ylabel('Experiment')
+axis equal; identityLine;
+
 vcNewGraphWin; hold on;
 X = 1:sum(indx); X = X(:);
 bar(X, [sim_algMean(indx)' exp_algMean(indx)']);
 errorbar([X-0.15 X+0.15], [sim_algMean(indx)' exp_algMean(indx)'], ...
     [sim_algStd(indx)' exp_algStd(indx)'], '.');
 grid on;
+
 %% End
